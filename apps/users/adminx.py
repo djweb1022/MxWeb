e75e9e27767dd44d247a70005fa6275580b3f78c
djweb1022 <forhappyday@126.com>
2018-10-25 20:03:31 +0800

    5-4 28:50 solve themes

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -3,8 +3,15 @@ __author__ = 'yfj'
 __date__ = '2018/10/25 12:55'
 
 import xadmin
+from xadmin import views
 
 from .models import EmailVerifyRecord,Banner
+
+
+class BaseSetting(object):
+    # 开启主题功能
+    enable_themes = True
+    use_bootswatch = True
 
 
 class EmailVerifyRecordAdmin(object):
@@ -23,3 +30,5 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+# xadmin.site.register(xadmin.views.BaseAdminView, BaseSetting)
