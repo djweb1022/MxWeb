6422b3c0dcbd0dddd7ab871e416558640201691c
djweb1022 <forhappyday@126.com>
2018-10-26 22:02:31 +0800

    6-3 finish

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,8 @@
+# -*- coding:utf-8 -*-
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
 
 from .models import UserProfile
 
@@ -10,7 +12,7 @@ from .models import UserProfile
 class CustomBackend(ModelBackend):
     def authenticate(self, username=None, password=None, **kwargs):
         try:
-            user = UserProfile.objects.get(username=username)
+            user = UserProfile.objects.get(Q(username=username) | Q(email=username))
             if user.check_password(password):
                 return user
         except Exception as e:
@@ -27,6 +29,6 @@ def user_login(request):
             login(request, user)
             return render(request, 'index.html')
         else:
-            return render(request, 'login.html', {})
+            return render(request, 'login.html', {'msg': '用户名或密码错误！'})
     elif request.method == 'GET':
         return render(request, 'login.html', {})
