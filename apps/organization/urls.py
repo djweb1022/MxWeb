4d5086e2c13cbb2dc29b04bd18477a4eb7f009bc
djweb1022 <forhappyday@126.com>
2018-11-03 16:30:35 +0800

    7-8 video 29:44

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -4,10 +4,10 @@ __date__ = '2018/11/3 14:59'
 
 from django.conf.urls import url, include
 
-from organization.views import OrgView
+from organization.views import OrgView, AddUserAskView
 
 urlpatterns = [
     # 课程机构列表页
     url(r'^list/$', OrgView.as_view(), name='org_list'),
-    url(r'^add_ask/$')
+    url(r'^add_ask/$', AddUserAskView.as_view(), name='add_ask')
 ]
