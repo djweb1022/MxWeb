88bf36b514f69c948e69c9e016b283467ddac2eb
djweb1022 <forhappyday@126.com>
2018-11-03 20:15:08 +0800

    7-8 finish problem:ajax double commit

--- apps/organization/forms.py
+++ apps/organization/forms.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 __author__ = 'yfj'
 __date__ = '2018/11/3 14:46'
+import re
 from django import forms
 
 from operation.models import UserAsk
@@ -16,3 +17,14 @@ class UserAskForm(forms.ModelForm):
     class Meta:
         model = UserAsk  # 继承UserAsk的form
         fields = ['name', 'mobile', 'course_name']
+
+    def clean_mobile(self):
+        """验证手机号码是否合法(正则表达式)"""
+        mobile = self.cleaned_data['mobile']
+        REGEX_MOBILE = "^1[358]\d{9}$|^147\d{8}$|^176\d{8}$"
+        p = re.compile(REGEX_MOBILE)
+        if p.match(mobile):
+            return mobile
+        else:
+            raise forms.ValidationError(u"手机号码非法", code="mobile_invalid")
+
