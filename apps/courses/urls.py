d1ce890b9ec953a322fa1e49b47616abee50c41e
djweb1022 <forhappyday@126.com>
2018-12-15 16:27:45 +0800

    8-4 finish

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -3,7 +3,7 @@ __author__ = 'yfj'
 __date__ = '2018/11/5 10:26'
 
 from django.conf.urls import url, include
-from courses.views import CourseListView, CourseDetailView
+from courses.views import CourseListView, CourseDetailView, CourseInfoView
 
 
 urlpatterns = [
@@ -11,4 +11,6 @@ urlpatterns = [
     url(r'^list/$', CourseListView.as_view(), name='course_list'),
     # 课程详情页
     url(r'^detail/(?P<course_id>\d+)/$', CourseDetailView.as_view(), name='course_detail'),
+
+    url(r'^info/(?P<course_id>\d+)/$', CourseInfoView.as_view(), name='course_info'),
 ]
