bfcab75441d3e3252471ec95c5048f7eae97fae9
djweb1022 <forhappyday@126.com>
2018-12-24 19:07:11 +0800

    12-1-1 finish

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -70,7 +70,7 @@ class Video(models.Model):
     lesson = models.ForeignKey(Lesson, verbose_name=u'章节')
     name = models.CharField(max_length=100, verbose_name=u'视频名')
     learn_times = models.IntegerField(default=0, verbose_name=u'学习时长(分钟数)')
-    url = models.CharField(max_length=200, default='', verbose_name=u'访问地址')
+    url = models.CharField(max_length=300, default='', verbose_name=u'访问地址')
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
 
     class Meta:
