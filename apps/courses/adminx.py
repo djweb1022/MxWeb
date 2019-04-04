a9b73ade6610dc7df180b3c49b6c18b483c13a1d
djweb1022 <forhappyday@126.com>
2019-04-04 14:46:31 +0800

    13-4 finish

--- apps/courses/adminx.py
+++ apps/courses/adminx.py
@@ -4,7 +4,17 @@ __date__ = '2018/10/25 15:51'
 
 import xadmin
 
-from .models import Course, Lesson, Video, CourseResource
+from .models import Course, Lesson, Video, CourseResource, BannerCourse
+
+
+class LessonInline(object):
+    model = Lesson
+    extra = 0
+
+
+class CourseResourceInline(object):
+    model = CourseResource
+    extra = 0
 
 
 class CourseAdmin(object):
@@ -13,11 +23,34 @@ class CourseAdmin(object):
     list_filter = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students']
     ordering = ['-click_nums']
 
+    # 将添加课程与添加章节、添加课程资源放在同一页面
+    inlines = [LessonInline, CourseResourceInline]
+
     # 设置为只读，数据不可更改
     # readonly_fields = ['click_nums']
 
     # 设置为不可见
     # exclude = ['click_nums']
+
+    def queryset(self):
+        qs = super(CourseAdmin, self).queryset()
+        qs = qs.filter(is_banner=False)
+        return qs
+
+
+# 格式和CourseAdmin相同
+class BannerCourseAdmin(object):
+    list_display = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'click_nums']
+    search_fields = ['name', 'desc', 'detail', 'degree', 'students']
+    list_filter = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students']
+    ordering = ['-click_nums']
+    inlines = [LessonInline, CourseResourceInline]
+
+# 运用重载仅显示轮播课程数据
+    def queryset(self):
+        qs = super(BannerCourseAdmin, self).queryset()
+        qs = qs.filter(is_banner=True)
+        return qs
 
 
 class LessonAdmin(object):
@@ -39,6 +72,7 @@ class CourseResourceAdmin(object):
 
 
 xadmin.site.register(Course, CourseAdmin)
+xadmin.site.register(BannerCourse, BannerCourseAdmin)
 xadmin.site.register(Lesson, LessonAdmin)
 xadmin.site.register(Video, VideoAdmin)
 xadmin.site.register(CourseResource, CourseResourceAdmin)
