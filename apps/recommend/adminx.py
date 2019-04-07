458dca960ad403e42326df5fcc76216d924ef334
djweb1022 <forhappyday@126.com>
2019-04-07 17:19:45 +0800

    重载save_models，让用户和课程的整数型ID与外键ID始终保持一致

--- apps/recommend/adminx.py
+++ apps/recommend/adminx.py
@@ -12,5 +12,15 @@ class UserRatingAdmin(object):
     search_fields = ['id_int_user', 'id_int_course', 'user', 'course', 'rating']
     list_filter = ['id_int_user', 'id_int_course', 'user', 'course', 'rating', 'add_time']
 
+    def save_models(self):
+        # 重载save_models，让用户和课程的整数型ID与外键ID始终保持一致
+        obj = self.new_obj
+        # obj.save()
+        if obj.id_int_user != obj.user.id:
+            obj.id_int_user = obj.user.id
+        if obj.id_int_course != obj.course.id:
+            obj.id_int_course = obj.course.id
+        obj.save()
+
 
 xadmin.site.register(UserRating, UserRatingAdmin)
